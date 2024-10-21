ae3cc3823dcb3c01a303ca6493b449a7cc66f4e0
Dahami Nethsarani <dahaminethsarani2000@gmail.com>
2024-10-21 10:00:06 +0530

    updated to designed model

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
+import Sidebar from './components/sidebar/Sidebar';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Content from './components/content/Content'; 
+import Steam from './pages/steam/Steam';
+import Water from './pages/Water/Water';
+import Fuel from './pages/fuel/Fuel';
+import Comair from './pages/comair/Comair';
+import CO2 from './pages/co2/Co2';
+import SteamTrend from './pages/steamtrend/Steamtrend'
+import Kpi from './pages/kpi/Kpi'
+import Elec from './pages/elec/Elec';
+/* import Profile from './pages/profile/Profile'; */
+import Mypage from './pages/mypage/Mypage'
+import Tcontent from './pages/content/Content';
 
-
-const App = () => {
+export default function App() {
   return (
-    <Router>
-      <div className="app">
-        <Sidebar />
-        <div className="content">
-          <Routes>
-            
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
+    <div className="dashboard">
+      <Sidebar />
+      <div className="dashboard--content">
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="steam" element={<Steam />} />
+          <Route path="water" element={<Water />} />
+          <Route path="fuel" element={<Fuel />} />
+          <Route path="comair" element={<Comair />} />
+          <Route path="co2" element={<CO2 />} />
+          <Route path="steamtrend" element={<SteamTrend />} />
+          <Route path="elec" element={<Elec />} />
+          <Route path="kpi" element={<Kpi />} />
+ {/*          <Route path="profile" element={<Profile />} /> */}
+          <Route path="mypage" element={<Mypage />} />
+          <Route path="content" element={<Tcontent />} />
 
-export default App;
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+
+    </div>
+  );
+}
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter([
+
+  {
+    path: "*",
+    element: <App />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+);
